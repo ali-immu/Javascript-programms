9727951b699bc0a91db2127173c17d9a768a2adc
Imran <imranali08gec@gmail.com>
2026-09-15 17:30:33 +0530

    practice....makes perfect Insha Allah

--- first-repeating-char.js
+++ first-repeating-char.js
@@ -1,13 +1,15 @@
 function firstRepeatingChar(str) {
-  for (let i = 0; i < str.length; i++) {
-    for (let j = i + 1; j < str.length; j++) {
-      if (str[i] == str[j]) {
-        return `The first repeating character is ${str[i]}`;
-      }
+  const seen = new Set();
+
+  for (const ch of str) {
+    if (seen.has(ch)) {
+      return `The first repeating character is ${ch}`;
     }
+    seen.add(ch);
   }
+
   return "No repeating character";
 }
 
-console.log(firstRepeatingChar("Hello"));
-console.log(firstRepeatingChar("Imran Ali"));
+console.log(firstRepeatingChar("Hello"));      // l
+console.log(firstRepeatingChar("Imran Ali"));  // No repeating character
